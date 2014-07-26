34207621f1722525aa38d3755369bcd63ffaf8fd
herrkaefer <gloolar@gmail.com>
2014-07-26 18:06:03 +0800

    bug fix of acceleration computation

--- smooth_driving.js
+++ smooth_driving.js
@@ -1,6 +1,28 @@
-// ref: [DeviceOrientation Event Specification](http://w3c.github.io/deviceorientation/spec-source-orientation.html)
+// Copyright 2014 LIU Yang <gloolar@gmail.com>
 
 var degtorad = Math.PI / 180; // Degree-to-Radian conversion
+
+var alpha, beta, gamma; // device orientation
+var ax, ay, az; // acceleration in body frame
+var a_earth; // accelration in earth frame
+var a_tn; // a_earth decomposed according to heading
+var heading, speed; // do not work on iphone4s
+var latitude = null, longitude = null;
+var last_latitude, last_longitude;
+var dt = 0.5; // time interval
+var a_abs; // absolute value of acceleration
+var last_a_abs; // last a_abs, used for for compute da
+var da; // derivative of acceleration
+var distance_from_start = 0; // driving distance from start
+var time_from_start = 0; // driving distance from start
+var time_past = 0;
+var min_distance = 3; // minimal distance to drive (km)
+var max_score = 100;
+var da_threshold = 1; // threshold of da to detect bad point
+var bad_da_past = 0;
+var rau = 1;
+var score = 0;
+
 
 // get rotation matrix ZXY from device body frame to earth frame. v_earth = ZXY * v_body.
 // from http://w3c.github.io/deviceorientation/spec-source-orientation.html
@@ -16,10 +38,6 @@ function getRotationMatrix( alpha, beta, gamma ) {
   var sX = Math.sin( _x );
   var sY = Math.sin( _y );
   var sZ = Math.sin( _z );
-
-  //
-  // ZXY rotation matrix construction.
-  //
 
   var m11 = cZ * cY - sZ * sX * sY;
   var m12 = - cX * sZ;
@@ -100,13 +118,9 @@ $(function () {
                         var series_a1 = this.series[1];
                         setInterval(function() {
                             var x = (new Date()).getTime(); // current time
-                                // a = Math.random();
-                                // a1 = a + Math.random();
-                                //get_acceleration();
-                                var a = get_absolute_acceleration();
-
-                                series_a.addPoint([x, a], true, true);
-                                series_a1.addPoint([x, a], true, true);
+                                step();
+                                series_a.addPoint([x, a_abs], true, true);
+                                series_a1.addPoint([x, da], true, true);
                               }, 500);
                       }
                     }
@@ -182,13 +196,6 @@ $(function () {
 });
 
 
-var alpha, beta, gamma;
-var ax, ay, az;
-var a_earth, a_tn;
-var heading, speed;
-var latitude, longitude;
-
-
 function deviceOrientationListener(event) {
   alpha = event.alpha;
   beta = event.beta;
@@ -198,6 +205,7 @@ function deviceOrientationListener(event) {
   document.getElementById("beta").innerHTML = Math.round(beta);
   document.getElementById("gamma").innerHTML = Math.round(gamma);
 }
+
 
 function deviceMotionListener(event) {
   ax = event.acceleration.x;
@@ -210,11 +218,14 @@ function deviceMotionListener(event) {
 }
 
 
-
-function get_decomposed_acceleration() {
+function transform_acceleration() {
   a_earth = body_to_earth(alpha, beta, gamma, ax, ay, az);
-  get_location();
-  a_tn = tangential_and_normal_decomposition(a_earth.x, a_earth.y, heading)
+  if (heading != null) {
+    a_tn = tangential_and_normal_decomposition(a_earth.x, a_earth.y, heading)
+  }
+  else {
+    console.log("heading is null");
+  }
 
   document.getElementById("a_earth_x").innerHTML = Math.round(a_earth.x);
   document.getElementById("a_earth_y").innerHTML = Math.round(a_earth.y);
@@ -222,10 +233,51 @@ function get_decomposed_acceleration() {
 }
 
 
-function get_absolute_acceleration() {
-  return Math.sqrt(ax*ax+ay*ay+az*az);
+function step() {
+
+  // compute_horizontal_acceleration
+  last_a_abs = a_abs;
+  a_earth = body_to_earth(alpha, beta, gamma, ax, ay, az);
+  a_abs = Math.sqrt(a_earth.x*a_earth.x + a_earth.y*a_earth.y);
+  da = (a_abs - last_a_abs) / dt;
+
+  // if (Math.abs(da) > da_threshold) {
+  //   bad_da_past += (Math.abs(da) - da_threshold) * dt; 
+  // }
+
+  // time_from_start += dt;
+
+  // if (Math.abs(latitude-last_latitude) > 1e-13 or Math.abs(longitude-last_longitude) > 1e-13) {
+    
+  //   distance_past = compute_distance(last_latitude, last_longitude, latitude, longitude);
+
+  //   // distance_from_start += last_distance;
+  //   // time_passed_from_last_position
+
+  //   if (distance_from_start < min_distance) {
+  //     max_score_past = distance_past * max_score / min_distance;
+  //     score += min(0,  1 - (rau * bad_da_past) / (2*da_threshold*time_past)) * max_score_past;
+
+  //   }
+  //   else {
+  //     // max_score_past = (max_score - score) / min_distance;
+  //   }
+
+  //   time_past = 0;
+  //   last_latitude = latitude;
+  //   last_longitude = longitude;
+  //   bad_da_past = 0;
+
+  // }
+  // else {
+  //   time_past += dt;
+
+  // }
+
 }
 
+
+// set event handlers
 if (window.DeviceOrientationEvent && window.DeviceMotionEvent && navigator.geolocation) {
 
   window.addEventListener("deviceorientation", deviceOrientationListener);
@@ -233,6 +285,10 @@ if (window.DeviceOrientationEvent && window.DeviceMotionEvent && navigator.geolo
   navigator.geolocation.watchPosition(function(position){
     heading = position.coords.heading;
     speed = position.coords.speed;
+    if (last_latitude == null || last_longitude == null) {
+      last_latitude = position.coords.latitude;
+      last_longitude = position.coords.longitude;  
+    }
     latitude = position.coords.latitude;
     longitude = position.coords.longitude;
 
@@ -244,11 +300,9 @@ if (window.DeviceOrientationEvent && window.DeviceMotionEvent && navigator.geolo
   })
 
   document.getElementById("support").innerHTML = "support";
-    // console.log("suppport.");
-  } 
-
-  else {
-    alert("Sorry, your browser doesn't support Device Orientation or Motion or geolocation.");
-    console.log("do not suppport.");
-  }
+} 
+else {
+  alert("Sorry, your browser doesn't support Device Orientation or Motion or geolocation.");
+  console.log("Brower does not suppport.");
+}
 
